cc715391c660c57b5c4ce3f0b0d4410f363c7c87
Angel <75736079+bryanind195@users.noreply.github.com>
2022-10-28 16:29:04 -0500

    se agrega barra de busqueda , falta configurar

--- pokemons/src/Header.js
+++ pokemons/src/Header.js
@@ -3,10 +3,20 @@ import React from "react";
 function Header({titulo}){
     return(
         <nav>
+            
             <div className="nav-wrapper light-blue darken-2">
                 <a href="#!" className="brand-logo">{titulo} </a>
             </div>
+
+            <div class="SearchDiv">
+                <form action="" onsubmit="SearchPokemon(event)" >
+                <input type="text" name="searchQuery" required /> <input
+            type="submit" name="searchBtn" title="search" value="search"/>
+    </form>
+</div>
+            
         </nav>
+        
     )
 }
 
